2101caaa93c10b3744c9999117ab94cfef83218c
botchedpunchy <71351127+milespunch@users.noreply.github.com>
2021-08-03 13:27:21 +1000

    Add files via upload

---
+++ mqtt_gui.py
@@ -0,0 +1,66 @@
+### SPECIAL SHIT ###
+import paho.mqtt.client as mqtt
+from tkinter import *
+
+### INSTANCES AND DECLARATIONS AND OTHER THINGS ###
+root = Tk()
+client =mqtt.Client("")
+#client.connect('YasawaPi')
+host = StringVar()
+
+### FUNCTIONS ###
+def ON1():
+    client.publish("study/relay0","ON")
+    print("SENT ON")
+def OFF1():
+    client.publish("study/relay0","OFF")
+    print("SENT OFF")
+def ON2():
+    client.publish("study/relay1","ON")
+    print("SENT ON")
+def OFF2():
+    client.publish("study/relay1","OFF")
+    print("SENT OFF")
+def ON3():
+    client.publish("study/relay2","ON")
+    print("SENT ON")
+def OFF3():
+    client.publish("study/relay2","OFF")
+    print("SENT OFF")
+def ON4():
+    client.publish("study/relay3","ON")
+    print("SENT ON")
+def OFF4():
+    client.publish("study/relay3","OFF")
+    print("SENT OFF")
+def RECONNECT():
+    client.connect(host.get())
+
+### TKINTER TEXT & ENTRY DECLARATIONS ###
+hostEntry = Entry(width=20, textvariable=host)
+hostEntry.grid(row=0,column=0, columnspan=3)
+
+### SPECIAL TKINTER BUTTON DECLARATIONS ###
+# CONNECT BUTTON
+connectButton = Button(root, text="CONNECT", width='7',command=RECONNECT)
+connectButton.grid(row=0,column=3)
+# STBD/1
+r1ON = Button(root, text="1 ON", command=ON1, width='7')
+r1ON.grid(row=2,column=0)
+r1OFF = Button(root, text="1 OFF", command=OFF1, width='7')
+r1OFF.grid(row=3,column=0)
+# STBD/2
+r2ON = Button(root, text="2 ON", command=ON2, width='7')
+r2ON.grid(row=2,column=1)
+r2OFF = Button(root, text="2 OFF", command=OFF2, width='7')
+r2OFF.grid(row=3,column=1)
+# STBD/3
+r3ON = Button(root, text="3 ON", command=ON3, width='7')
+r3ON.grid(row=2,column=2)
+r3OFF = Button(root, text="3 OFF", command=OFF3, width='7')
+r3OFF.grid(row=3,column=2)
+# STBD/4
+r4ON = Button(root, text="4 ON", command=ON4, width='7')
+r4ON.grid(row=2,column=3)
+r4OFF = Button(root, text="4 OFF", command=OFF4, width='7')
+r4OFF.grid(row=3,column=3)
